eb85b98665aa073a1040d921c8e13d66af73fb73
odhisLuka <luketrevor03@gmail.com>
2023-09-20 16:57:28 +0300

    delete products from Cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [
+export let cart = [
   {
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2,
@@ -31,4 +31,15 @@ export function addToCart(productId) {
       quantity: selectorQty,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (productId !== cartItem.productId) {
+      newCart.push(cartItem);
+    }
+  });
+  cart = newCart;
 }
